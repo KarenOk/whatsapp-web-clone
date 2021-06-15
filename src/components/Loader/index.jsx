ff9002e75576eccf9762ee12cd948ad0c073e1a6
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-06-15 19:04:20 +0100

    Update class names for loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -5,15 +5,15 @@ import Icon from "../Icon";
 const Loader = ({ done }) => {
 	return (
 		<div className="loader">
-			<div className="loader__main-icon-wrapper">
-				<Icon id="whatsapp" className="loader__main-icon" />
+			<div className="loader__logo-wrapper">
+				<Icon id="whatsapp" className="loader__logo" />
 			</div>
 			<div
 				className={`loader__progress ${done ? "loader__progress--done" : ""}`}
 			></div>
 			<h1 className="loader__title"> Whatsapp</h1>
 			<p className="loader__desc">
-				<Icon id="lock" className="loader__lock-icon" />
+				<Icon id="lock" className="loader__icon" />
 				End-to-end encrypted
 			</p>
 		</div>
