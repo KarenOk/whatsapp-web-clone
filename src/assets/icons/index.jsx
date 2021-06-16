e76d04688daed951b794d6aed401d0bf8e160374
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-06-16 13:51:49 +0100

    Implement  attach buttons

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -13,6 +13,179 @@ const icons = {
 			></path>
 		</svg>
 	),
+	attachCamera: (props) => (
+		<span data-testid="attach-camera" data-icon="attach-camera" class="_1SWzr">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 53 53"
+				width="53"
+				height="53"
+				{...props}
+			>
+				<defs>
+					<circle id="camera-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle>
+				</defs>
+				<clipPath id="camera-SVGID_2_">
+					<use xlinkHref="#camera-SVGID_1_" overflow="visible"></use>
+				</clipPath>
+				<g clip-path="url(#camera-SVGID_2_)">
+					<path
+						fill="#D3396D"
+						d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"
+					></path>
+					<path
+						fill="#EC407A"
+						d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"
+					></path>
+					<path fill="#D3396D" d="M17 24.5h15v9H17z"></path>
+				</g>
+				<g fill="#F5F5F5">
+					<path
+						id="svg-camera"
+						d="M27.795 17a3 3 0 0 1 2.405 1.206l.3.403a3 3 0 0 0 2.405 1.206H34.2a2.8 2.8 0 0 1 2.8 2.8V32a4 4 0 0 1-4 4H20a4 4 0 0 1-4-4v-9.385a2.8 2.8 0 0 1 2.8-2.8h1.295a3 3 0 0 0 2.405-1.206l.3-.403A3 3 0 0 1 25.205 17h2.59zM26.5 22.25a5.25 5.25 0 1 0 .001 10.501A5.25 5.25 0 0 0 26.5 22.25zm0 1.75a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7z"
+					></path>
+				</g>
+			</svg>
+		</span>
+	),
+	attachContacts: (props) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 53 53"
+			width="53"
+			height="53"
+			{...props}
+		>
+			<defs>
+				<circle id="contact-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle>
+			</defs>
+			<clipPath id="contact-SVGID_2_">
+				<use xlinkHref="#contact-SVGID_1_" overflow="visible"></use>
+			</clipPath>
+			<g clip-path="url(#contact-SVGID_2_)">
+				<path
+					fill="#0795DC"
+					d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"
+				></path>
+				<path
+					fill="#0EABF4"
+					d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"
+				></path>
+			</g>
+			<g fill="#F5F5F5">
+				<path
+					id="svg-contact"
+					d="M26.5 26.5A4.5 4.5 0 0 0 31 22a4.5 4.5 0 0 0-4.5-4.5A4.5 4.5 0 0 0 22 22a4.5 4.5 0 0 0 4.5 4.5zm0 2.25c-3.004 0-9 1.508-9 4.5v1.125c0 .619.506 1.125 1.125 1.125h15.75c.619 0 1.125-.506 1.125-1.125V33.25c0-2.992-5.996-4.5-9-4.5z"
+				></path>
+			</g>
+		</svg>
+	),
+	attachDocument: (props) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 53 53"
+			width="53"
+			height="53"
+			{...props}
+		>
+			<defs>
+				<circle id="document-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle>
+			</defs>
+			<clipPath id="document-SVGID_2_">
+				<use xlinkHref="#document-SVGID_1_" overflow="visible"></use>
+			</clipPath>
+			<g clip-path="url(#document-SVGID_2_)">
+				<path
+					fill="#5157AE"
+					d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"
+				></path>
+				<path
+					fill="#5F66CD"
+					d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"
+				></path>
+			</g>
+			<g fill="#F5F5F5">
+				<path
+					id="svg-document"
+					d="M29.09 17.09c-.38-.38-.89-.59-1.42-.59H20.5c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H32.5c1.1 0 2-.9 2-2V23.33c0-.53-.21-1.04-.59-1.41l-4.82-4.83zM27.5 22.5V18l5.5 5.5h-4.5c-.55 0-1-.45-1-1z"
+				></path>
+			</g>
+		</svg>
+	),
+	attachImage: (props) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 53 53"
+			width="53"
+			height="53"
+			{...props}
+		>
+			<defs>
+				<circle id="image-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle>
+			</defs>
+			<clipPath id="image-SVGID_2_">
+				<use xlinkHref="#image-SVGID_1_" overflow="visible"></use>
+			</clipPath>
+			<g clip-path="url(#image-SVGID_2_)">
+				<path
+					fill="#AC44CF"
+					d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"
+				></path>
+				<path
+					fill="#BF59CF"
+					d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"
+				></path>
+				<path fill="#AC44CF" d="M17 24.5h18v9H17z"></path>
+			</g>
+			<g fill="#F5F5F5">
+				<path
+					id="svg-image"
+					d="M18.318 18.25h16.364c.863 0 1.727.827 1.811 1.696l.007.137v12.834c0 .871-.82 1.741-1.682 1.826l-.136.007H18.318a1.83 1.83 0 0 1-1.812-1.684l-.006-.149V20.083c0-.87.82-1.741 1.682-1.826l.136-.007h16.364zm5.081 8.22l-3.781 5.044c-.269.355-.052.736.39.736h12.955c.442-.011.701-.402.421-.758l-2.682-3.449a.54.54 0 0 0-.841-.011l-2.262 2.727-3.339-4.3a.54.54 0 0 0-.861.011zm8.351-5.22a1.75 1.75 0 1 0 .001 3.501 1.75 1.75 0 0 0-.001-3.501z"
+				></path>
+			</g>
+		</svg>
+	),
+	attachRooms: (props) => (
+		<svg
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 53 53"
+			width="53"
+			height="53"
+			{...props}
+		>
+			<defs>
+				<circle id="rooms-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle>
+			</defs>
+			<clipPath id="rooms-SVGID_2_">
+				<use xlinkHref="#rooms-SVGID_1_" overflow="visible"></use>
+			</clipPath>
+			<g clip-path="url(#rooms-SVGID_2_)">
+				<path
+					fill="#396CD3"
+					d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"
+				></path>
+				<path
+					fill="#4079EC"
+					d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"
+				></path>
+				<path fill="#396CD3" d="M17 24.5h15v9H17z"></path>
+			</g>
+			<g fill="#F5F5F5">
+				<path
+					id="svg-rooms"
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M28.5 18a3.5 3.5 0 0 1 3.5 3.5v10a3.5 3.5 0 0 1-3.5 3.5h-10a3.5 3.5 0 0 1-3.5-3.5v-10a3.5 3.5 0 0 1 3.5-3.5h10zm-7.925 6.782a.804.804 0 0 0-1.045-.073l-.708.698-.297.33-.134.177a4.075 4.075 0 0 0 .433 5.265 4.07 4.07 0 0 0 2.886 1.198c.976 0 1.952-.349 2.73-1.05l.784-.777.077-.091.063-.102.045-.105a.797.797 0 0 0-.189-.824.806.806 0 0 0-1.045-.073l-.704.694-.123.116-.147.119-.15.105a2.5 2.5 0 0 1-3.099-.337 2.492 2.492 0 0 1-.12-3.392l.748-.756.076-.091.063-.102.046-.105a.794.794 0 0 0-.19-.824zm5.281-.637a.803.803 0 0 0-1.044-.073l-3.67 3.662-.078.091-.063.102-.045.105a.793.793 0 0 0 .754 1.056.805.805 0 0 0 .481-.16l3.671-3.662.076-.09.064-.102.045-.105a.795.795 0 0 0-.191-.824zm2.323-2.323a4.078 4.078 0 0 0-5.615-.149l-.785.777-.073.084-.062.098a.806.806 0 0 0 .14.941.803.803 0 0 0 1.043.072l.705-.693.127-.119.137-.111a2.494 2.494 0 0 1 3.257.227c.921.923.972 2.413.117 3.392l-.746.755-.072.084-.062.098a.8.8 0 0 0 .139.941.8.8 0 0 0 1.043.072l.709-.697.148-.156.145-.17a4.084 4.084 0 0 0-.295-5.446zm9.815-.188l.006.129v9.474c0 1.31-1.714 1.998-2.744 1.12l-.878-.847a2.828 2.828 0 0 1-.871-1.849l-.007-.19v-5.943c0-.765.316-1.498.879-2.039l.877-.846c.996-.848 2.631-.234 2.738.991z"
+				></path>
+			</g>
+		</svg>
+	),
 	back: (props) => (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Icon from "components/Icon";
 import avatar from "assets/images/profile-picture-girl-4.jpeg";
 
+// import emojiList from "emojis-list";
+
+// console.log(emojiList);
+// let index = emojiList.findIndex((emoji) => emoji === "😊");
+// console.log(index);
+// 1759 - 1940
+
 const Chat = () => {
+	const [showAttach, setShowAttach] = useState(false);
+
 	return (
 		<div className="chat">
 			<header className="chat__header">
@@ -29,12 +38,54 @@ const Chat = () => {
 			</header>
 			<div className="chat__content">
 				<div className="chat__bg"></div>
+				{/* <div className="chat__emojis-wrapper"></div> */}
 			</div>
 			<div className="chat__input-wrapper">
-				<Icon id="smiley" className="chat__input-icon" />
-				<Icon id="attach" className="chat__input-icon" />
+				<button aria-label="Emojis">
+					<Icon
+						id="smiley"
+						className="chat__input-icon chat__input-icon--highlight"
+					/>
+				</button>
+				<div className="pos-rel">
+					<button
+						aria-label="Attach"
+						onClick={() => setShowAttach(!showAttach)}
+					>
+						<Icon
+							id="attach"
+							className={`chat__input-icon ${
+								showAttach ? "chat__input-icon--pressed" : ""
+							}`}
+						/>
+					</button>
+
+					<div
+						className={`chat__attach ${
+							showAttach ? "chat__attach--active" : ""
+						}`}
+					>
+						<button className="chat__attach-btn" aria-label="Emojis">
+							<Icon id="attachRooms" className="chat__attach-icon" />
+						</button>
+						<button className="chat__attach-btn" aria-label="Attach">
+							<Icon id="attachContacts" className="chat__attach-icon" />
+						</button>
+						<button className="chat__attach-btn" aria-label="Record voice note">
+							<Icon id="attachDocument" className="chat__attach-icon" />
+						</button>
+						<button className="chat__attach-btn" aria-label="Attach">
+							<Icon id="attachCamera" className="chat__attach-icon" />
+						</button>
+						<button className="chat__attach-btn" aria-label="Record voice note">
+							<Icon id="attachImage" className="chat__attach-icon" />
+						</button>
+					</div>
+				</div>
 				<input className="chat__input" placeholder="Type a message" />
-				<Icon id="microphone" className="chat__input-icon" />
+				<button aria-label="Record voice note">
+					<Icon id="microphone" className="chat__input-icon" />
+				</button>
 			</div>
 		</div>
 	);
