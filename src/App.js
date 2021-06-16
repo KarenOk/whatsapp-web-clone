9804709696ecf170bae6e87ef9b46820eb8f484f
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-06-16 01:32:30 +0100

    Include msg for smaller screens

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 
 	return (
 		<div className="app">
+			<p className="app__mobile-message"> Only available on desktop 😊. </p>
 			<div className="app-content">
 				<Sidebar />
 				<Router>
