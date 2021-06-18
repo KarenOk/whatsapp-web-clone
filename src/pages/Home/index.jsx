8449aa2a264855928917cba2a3e8dd5efecd9ab8
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-06-19 00:18:24 +0100

    Implement dark mode for home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./styles/main.css";
 import Icon from "components/Icon";
-import introImg from "assets/images/intro-connection-light.jpg";
+import introImgLight from "assets/images/intro-connection-light.jpg";
+import introImgDark from "assets/images/intro-connection-dark.jpg";
 
 const Home = () => {
+	const darkTheme = document.body.classList.contains("dark-theme");
+
 	return (
 		<div className="home">
 			<div className="home__img-wrapper">
-				<img src={introImg} alt="" className="home__img" />
+				<img
+					src={darkTheme ? introImgDark : introImgLight}
+					alt=""
+					className="home__img"
+				/>
 			</div>
 
 			<h1 className="home__title"> Keep your phone connected </h1>
