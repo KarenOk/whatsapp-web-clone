8729980228391ed1584decdc94fed426a5be5cf0
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-09-06 19:47:13 +0100

    Update port for local backend

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -2,7 +2,7 @@ import { createContext, useContext } from "react";
 import io from "socket.io-client";
 
 const SOCKET_URL = window.location.origin.includes("localhost")
-	? "http://localhost:5001"
+	? "http://localhost:5000"
 	: "https://whatsapp-web-clone-backend.herokuapp.com/";
 
 const socket = io.connect(SOCKET_URL);
